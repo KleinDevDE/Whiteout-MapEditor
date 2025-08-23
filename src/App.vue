43e77c04ad3b47b9bbef3f6f080539f15a3d729f
Maximilian Klein <13904220+KleinDevDE@users.noreply.github.com>
2025-08-23 04:25:30 +0200

    feat: set DRAFT_ID from URL for draft saving functionality

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,16 @@
 <script setup lang="ts">
 import GridPainter from './components/GridPainter.vue'
+
+declare global {
+  interface Window {
+    DRAFT_ID?: string;
+  }
+}
+//If save/XY in URL, set window.DRAFT_ID to XY
+if (location.search.includes('/save/')) {
+  window.DRAFT_ID = location.search.split('/save/')[1];
+}
+
 </script>
 
 <template>
